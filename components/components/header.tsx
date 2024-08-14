967ce9b1380e72a81dc032c0f21d9c688b3c3aec
Ayush chauhan <ayush.chuahan.22cse@bmu.edu.in>
2024-08-14 17:30:38 +0530

    Added Navigation Bar

--- components/components/header.tsx
+++ components/components/header.tsx
@@ -1,7 +1,16 @@
+import Image from "next/image";
+import { NavigationMenuDemo } from "../client/navigation";
+
 export default function Header() {
   return (
     <header>
-      <h1>My App</h1>
+      <div className="w-full bg-black text-white py-1 flex justify-center font-sans font-medium">Welcome to AnimeXschool</div>
+      <div className="h-20 flex justify-center items-center">
+        <Image src="/logo.webp" alt="logo" width={178} height={200} />
+      </div>
+      <div className="py-2 flex justify-center mt-4">
+      <NavigationMenuDemo />
+      </div>
     </header>
   );
 }
