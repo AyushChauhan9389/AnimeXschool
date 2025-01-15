f3e131dbd105ebd83eebde4146f7d3911e0cad13
Ayush Chauhan <ayush.chuahan.22cse@bmu.edu.in>
2025-01-15 13:13:22 +0530

    fix: update image domains in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['logo.tg-api.me'],
+        domains: ['lelekart.com','www.lelekart.com'],
     },
 
 };
